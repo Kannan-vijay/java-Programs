38fa0c0674d017c9bc85bd68f4093906a925e089
Kannan-vijay <kannanvijay7339099916@gmail.com>
2024-09-30 08:51:52 +0530

    snake midification

--- snakepattern.java
+++ snakepattern.java
@@ -5,11 +5,11 @@ public class snakepattern {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if(i%2==0){
-                    System.out.printf("%3d",num);
+                    System.out.printf("%4d",num);
                     num++;
                 }
                 else{
-                    System.out.printf("%3d",num);
+                    System.out.printf("%4d",num);
                     num--;
                 }
             }
